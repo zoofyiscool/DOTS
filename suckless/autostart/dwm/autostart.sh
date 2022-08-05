877f1f421d6fb9500fb5fab9cdd3969895212dfa
zoofyiscool <75759609+zoofyiscool@users.noreply.github.com>
2022-08-05 23:26:07 +0000

    Delete suckless directory

--- suckless/autostart/dwm/autostart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-picom -b
-xrandr --output HDMI-A-0 --auto --left-of DisplayPort-0
-feh --bg-fill /home/ouxiez/wallpapers/mon1.png --bg-fill /extra/Wallpapers/firewatch-landscape-purple-sky-ho-1920x1080.jpg
-slstatus &
